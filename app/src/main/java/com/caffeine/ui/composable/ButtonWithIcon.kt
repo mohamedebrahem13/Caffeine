e9b6e69f142194a4a690e6fd799b613b09ac43db
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-08 13:07:31 +0300

    add shadow to button

--- app/src/main/java/com/caffeine/ui/composable/ButtonWithIcon.kt
+++ app/src/main/java/com/caffeine/ui/composable/ButtonWithIcon.kt
@@ -15,8 +15,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import com.caffeine.android.theme.TextDarkGray
 
@@ -27,10 +33,36 @@ fun ButtonWithIcon(
     icon: Painter,
     onClick: () -> Unit
 ) {
+    val shadowColor = Black.copy(alpha = 0.24f)
+    val shadowRadius = 12.dp
+    val shadowOffsetY = 6.dp
     Button(
         onClick = onClick,
         colors = ButtonDefaults.buttonColors(containerColor = TextDarkGray),
-        modifier = modifier.wrapContentSize(),
+        modifier = modifier.wrapContentSize()
+            .drawBehind {
+                val paint = Paint().asFrameworkPaint().apply {
+                    isAntiAlias = true
+                    color = shadowColor.toArgb()
+                    setShadowLayer(
+                        shadowRadius.toPx(),
+                        0f,
+                        shadowOffsetY.toPx(),
+                        shadowColor.toArgb()
+                    )
+                }
+                drawIntoCanvas {
+                    it.nativeCanvas.drawRoundRect(
+                        0f,
+                        0f,
+                        size.width,
+                        size.height,
+                        100.dp.toPx(),
+                        100.dp.toPx(),
+                        paint
+                    )
+                }
+            },
         shape = RoundedCornerShape(100.dp),
         contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
     ) {
